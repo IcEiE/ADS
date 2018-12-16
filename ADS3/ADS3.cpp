3612787a52e9b4dbb50098817eae35e5cbb1b5d8
Issa Hijazi <issa.hijazi92@gmail.com>
2018-12-16 12:35:11 +0100

    Traversals

--- ADS3/ADS3.cpp
+++ ADS3/ADS3.cpp
@@ -2,29 +2,28 @@
 #include <iostream>
 #include "InsertionSort.h"
 #include "ModBucketSort.h"
+#include "BinaryTree.h"
 
 int main()
 {
 	std::cout << "MENU \n \n";
 	std::cout << "1: Insertion Sort\n";
 	std::cout << "2: Modified Bucket Sort\n";
+	std::cout << "3: Binary Tree\n";
 	std::cout << "\nI choose: ";
 	int choice;
-	InsertionSort *is;
-	ModBucketSort *mbs;
 	std::cin >> choice;
-	switch (choice)
-	{
-	case 1:
-		is = new InsertionSort();
+	if (choice == 1) {
+		InsertionSort *is = new InsertionSort();
 		delete is;
-		break;
-	case 2:
-		mbs = new ModBucketSort();
+	}
+	else if (choice == 2) {
+		ModBucketSort *mbs = new ModBucketSort();
 		delete mbs;
-		break;
-	default:
-		break;
+	}
+	else if (choice == 3) {
+		BinaryTree *bt = new BinaryTree();
+		delete bt;
 	}
 	std::cout << "\n\n\n";
 }
